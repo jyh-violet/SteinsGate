8576e72a1ec9472b1c6b84396fbc2f2fa91551bb
acmore <acmoretxj@163.com>
2016-03-13 16:36:24 +0800

    Initial commit

---
+++ app/src/main/java/com/unknown/main/Entry.java
@@ -0,0 +1,15 @@
+package com.unknown.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.unknown.R;
+
+public class Entry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entry);
+    }
+}
